4229551cd5ae89341af5424b222e2e9b8c325072
Berkan Kaygusuz <74183528+KaygusuzBK@users.noreply.github.com>
2021-03-14 15:02:48 +0300

    Delete Person
    Delete fonksiyonununu ekleyerek rehberde kayıtlı bulunan kişileri istediğimiz zaman silebiliyoruz

--- classes.py
+++ classes.py
@@ -12,8 +12,6 @@ class Person():
         ContactList.addToList(self)
         ContactList.listItems()
 
-
-
 class PersonList():
     def __init__(self):
         self.personList = []
@@ -25,4 +23,17 @@ class PersonList():
         for item in self.personList:
             print(item.name, item.surname, item.phoneNumber )
 
+    def deletePerson(self):        
+        deletePersonName = input("Silmek istediğiniz ismi giriniz: ")
+        for item in ContactList.personList:
+            if  deletePersonName == item.name:
+                ContactList.personList.remove(item)
+                return
+            else:
+                print("Kişi bulunamadı")
+                return
+
 ContactList = PersonList()
+
+p1 = Person()
+p1.addToPerson()
